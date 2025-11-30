f36b041fac4baffb9566c9fe91fa79c87a2495a8
Vinicius <Viniciusr.camargo@gmail.com>
2025-11-30 07:53:06 -0300

    Aprendendo js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2]; 
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+  // verificaPalavrasDuplicadas(texto);
+  quebraemParagrafos(texto);
+})
+
+function quebraemParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) =>{
+      return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+    
+}
+//criar um array com palavras
+
+function verificaPalavrasDuplicadas(texto){
+    const listaPalavras = texto.split(' ');
+    const resultado = {}
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1; 
+    })
+    return resultado;
+}
