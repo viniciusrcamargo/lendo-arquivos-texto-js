68878bec26542ac2c66bc8bf517222737fd215d7
Vinicius <Viniciusr.camargo@gmail.com>
2025-12-05 12:03:54 -0300

    Aprendendo js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,46 @@
 const fs = require('fs');
 
 const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
+const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  // verificaPalavrasDuplicadas(texto);
-  quebraemParagrafos(texto);
+  try {
+    contaPalavras(texto);
+  }catch (erro) {
+    console.log('Erro gerado', erro);
+    
+  }
 })
 
-function contaPalavras(texto){
-    const listaPalavras = texto.split(' ');
-    const resultado = {}}
-
-    function extraiParágrafos(texto){
-      return texto.toLowerCase().split('\n');
-    }
-
-function quebraemParagrafos(texto){
-    //se tiver conteúdo true, se não false 
-    const contagem = paragrafos.flatMap((paragrafo) =>{
-      if(!paragrafo) return [];
-      return verificaPalavrasDuplicadas(paragrafo);
-    })
-    console.log(contagem); 
+function contaPalavras(texto) {
+  //se tiver conteúdo true, se não false 
+  const paragrafos = extraiParágrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
 }
+
+function extraiParágrafos(texto) {
+  return texto.toLowerCase().split('\n');
+}
+
+
 //criar um array com palavras
 
-function limpaPalavras(palavra){
+function limpaPalavras(palavra) {
   return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
 }
 
-function verificaPalavrasDuplicadas(texto){
-    const listaPalavras = texto.split(' ');
-    const resultado = {}
-    listaPalavras.forEach(palavra => {
-      if(palavra.length >= 3) {
-        const palavraLimpa = limpaPalavras(palavra);
-        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1; 
-      }
-    })
-    return resultado;
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {}
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  })
+  return resultado;
 }
